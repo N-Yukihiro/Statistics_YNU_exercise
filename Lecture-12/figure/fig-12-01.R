e397df3144c7375c1ad43bdf74f436dc9858a550
NAKAJIMA, Yukihiro <yukihiro.618.soa@gmail.com>
2023-07-05 14:18:45 +0900

    Upload Lecture-12

---
+++ Lecture-12/figure/fig-12-01.R
@@ -0,0 +1,27 @@
+library(pacman)
+p_load(withr, dplyr, ggplot2)
+
+dat <- with_seed(2718,
+                 data.frame(x = rnorm(15, mean = 5)) |> 
+                   mutate(y = 2 * x + rnorm(15),
+                          c = sample(1:3, 
+                                     size    = 15,
+                                     replace = TRUE) |> 
+                            as.factor()))
+dat |> 
+  ggplot() +
+  aes(x = x, y = y) +
+  geom_smooth(method      = "lm",
+              se          = FALSE,
+              alpha       = 0.1,
+              colour      = "orange",
+              show.legend = FALSE) +
+  geom_smooth(aes(colour  = c),
+              method      = "lm",
+              se          = FALSE,
+              fullrange   = TRUE,
+              show.legend = FALSE,
+              alpha       = 0.1) +
+  geom_point() +
+  scale_colour_manual(values = c("red", "green", "blue")) +
+  theme_classic()
