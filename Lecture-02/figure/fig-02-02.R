498f9c5d90079e9a495589b67526cc69ad12d58d
NAKAJIMA, Yukihiro <yukihiro.618.soa@gmail.com>
2024-04-16 18:47:00 +0900

    Update fig-02-02.R

--- Lecture-02/figure/fig-02-02.R
+++ Lecture-02/figure/fig-02-02.R
@@ -1,13 +1,10 @@
 ## 夜景
-pacman::p_load(ggplot2)
 ggplot(data = NULL) +
-  aes(xmin = -4, 
-      xmax = 4) +
-  geom_point(aes(x = 1, 
-                 y = 0.4),
-             size   = 25,
+  aes(xmin = -4, xmax = 4) +
+  geom_point(aes(x = 1, y = 0.4),
+             size = 25,
              colour = "yellow") +
-  geom_point(data   = 
+  geom_point(data = 
                data.frame(
                  x = runif(n   = 50,
                            min = -4,
@@ -15,14 +12,13 @@ ggplot(data = NULL) +
                  y = runif(n   = 50,
                            min = 0.2,
                            max = 0.5)),
-             aes(x = x, 
-                 y = y),
+             aes(x = x, y = y),
              size   = 2,
              colour = "yellow") +
   stat_function(fun  = dnorm,
                 args = list(sd = 1.5),
                 aes(ymin = 0, 
-                    ymax = ..y..),
+                    ymax = after_stat(y)),
                 geom = "ribbon",
                 fill = "darkblue") +
   theme_void() +
