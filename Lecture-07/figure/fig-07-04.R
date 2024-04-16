a802d56caf31c991d9fb5061469f3b729c668286
NAKAJIMA, Yukihiro <yukihiro.618.soa@gmail.com>
2024-04-16 17:17:26 +0900

    Delete Lecture-07 directory

--- Lecture-07/figure/fig-07-04.R
+++
@@ -1,9 +0,0 @@
-qt(p  = 0.025,
-   df = 10:200) |> 
-  as_tibble() |> 
-  mutate(size = 10:200,
-         diff = qnorm(0.025) - value) |> 
-  ggplot() +
-  aes(x = size,
-      y = diff) +
-  geom_path()
